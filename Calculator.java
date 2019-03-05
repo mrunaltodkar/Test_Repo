53407c2ee956e78395c8fa4481b1df04f2136508
Todkar <mrunal-sunilkumar.todkar@capgemini.com>
2019-03-05 10:42:30 +0530

    Added subtraction() funtion

--- Calculator.java
+++ Calculator.java
@@ -6,4 +6,7 @@ public class Calculator{
 	public static void addition(int number1, int number2){
 		System.out.println("Addition = " + (number1 + number2));
 	}
+	public static void subtraction(int number1, int number2){
+		System.out.println("Subtraction = " + (number1 - number2));
+	}
 }
