de3c856a40e2d161e455e046044cfa30b83558d8
Todkar <mrunal-sunilkumar.todkar@capgemini.com>
2019-03-05 10:51:31 +0530

    Added division() function

--- Calculator.java
+++ Calculator.java
@@ -6,4 +6,10 @@ public class Calculator{
 	public static void addition(int number1, int number2){
 		System.out.println("Addition = " + (number1 + number2));
 	}
+	public static void division(int number1, int number2){
+	   if(number2 == 0)
+		System.out.println("Division should not be 0");
+	   else
+		System.out.println("Division = " + (number1 / number2));
+	}
 }
